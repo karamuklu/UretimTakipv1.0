894dbf244d8161aeeee2f45fa0de52b37056191c
mustafakaramuklu <mustafakaramuklu@gmail.com>
2022-03-04 12:11:33 +0300

    v1.1

---
+++ INF_URETIMTAKIP_EntitiesLayer/TBLSTHAR.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF_URETIMTAKIP_EntitiesLayer
+{
+	public class TBLSTHAR
+	{
+		public string STOK_KODU { get; set; }
+		public string FISNO { get; set; }
+		public int DEPO_KODU { get; set; }
+		public int INCKEYNO { get; set; }
+	}
+}
---
+++ INF_URETIMTAKIP_EntitiesLayer/URUNSIRA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF_URETIMTAKIP_EntitiesLayer
+{
+	public class URUNSIRA
+	{
+		public string ISEMRINO { get; set; }
+		public int MIKTAR { get; set; }
+		public int ADET { get; set; }
+	}
+	 
+}
